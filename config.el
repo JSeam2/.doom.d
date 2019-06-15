99ef89ecaee2485578f5153f65e34d56144d2c8d
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2019-06-16 02:40:53 +0800

    Added new snippets and config

--- config.el
+++ config.el
@@ -11,5 +11,30 @@
 ;; Clean up
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
+;; snippets
+(use-package emacs-snippets
+  :load-path "snippets"
+  :after yasnippet)
+
+;; Markdown mode
+(use-package markdown-mode
+  :custom
+  (markdown-hide-markup nil)
+  (markdown-bold-underscore t)
+  (markdown-italic-underscore t)
+  (markdown-header-scaling t)
+  (markdown-indent-function t)
+  (markdown-enable-math t)
+  (markdown-hide-urls nil)
+  :mode "\\.md\\'")
+
+(setq markdown-xhtml-header-content
+      (concat "<script type=\"text/javascript\" async"
+              " src=\"https://cdnjs.cloudflare.com/ajax/libs/mathjax/"
+              "2.7.1/MathJax.js?config=TeX-MML-AM_CHTML\">"
+              "</script>"))
+
+(use-package markdown-toc)
+
 ;; Additional stuff
 (load! "+ui")
